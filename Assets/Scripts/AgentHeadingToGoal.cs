c048bcdd0cb65f2e5ae76710647cb9bd04aaeffe
st1217 <ece01217@uowm.gr>
2020-12-15 17:54:39 +0200

    navmesh agent
    έκανα μια πρώτη απόπειρα σε εχθρική μονάδα. Προς το παρόν κινείται από από την αρχή μέχρι το τέλος της πίστας και αγνοεί μονάδες που συναντά. Τα αντικείμενα που πρόσθεσα στην σκηνή είναι τα AttemptAtAnEnemy και Goal και ένα απλό script

---
+++ Assets/Scripts/AgentHeadingToGoal.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AgentHeadingToGoal : MonoBehaviour
+{
+    public Transform goal;
+  
+    void Start()
+    {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.destination = goal.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
