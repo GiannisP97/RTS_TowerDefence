15a1ae469a74203898e1d77a8b781401de516793
st1217 <ece01217@uowm.gr>
2020-12-17 17:02:27 +0200

    navmesh script
    καινούριο script για τους navmesh agents(εχθρούς)

--- Assets/Scripts/AgentHeadingToGoal.cs
+++ Assets/Scripts/AgentHeadingToGoal.cs
@@ -5,17 +5,42 @@ using UnityEngine.AI;
 
 public class AgentHeadingToGoal : MonoBehaviour
 {
-    public Transform goal;
-  
+   // public Transform goal;
+    public GameObject[] paths;
+    public NavMeshAgent agent;
+
+    private int startingPath = 0;
+    private int pathLength = 0;
+
     void Start()
     {
-        NavMeshAgent agent = GetComponent<NavMeshAgent>();
-        agent.destination = goal.position;
+        startingPath = 0;
+        pathLength = paths.Length;
+        agent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (startingPath < pathLength)
+        {
+            if (Vector3.Distance(paths[startingPath].transform.position, gameObject.transform.position) < 1)
+            {
+                if( startingPath == pathLength - 1)
+                {
+                    agent.isStopped=true;
+                } else
+                {   Debug.Log("checkpoint");
+                    startingPath++;
+                }
+            }
+        }
         
+        agent.SetDestination(paths[startingPath].transform.position);
+
+        if (agent.isStopped){
+            Debug.Log("I stopped");
+        }
+
     }
 }
